e9bf55d5fd6c466eb1a1eb29bb4bd7aef03e9c9b
CL <bkcaleb@naver.com>
2018-01-23 19:07:52 -0800

    a
    a

--- Sort/Sorts.java
+++ Sort/Sorts.java
@@ -51,14 +51,14 @@ public class Sorts{
           // gives the effective size of the unsorted  array .
 
             for(int j = i+1; j < list.size() ; j++ ) {
-                if(list.get(j) < list.get( minimum))  {                //finds the minimum element
+                if(list.get(j) < list.get(minimum))  {                //finds the minimum element
                 minimum = j ;
                 }
              }
           // putting minimum element on its proper position.
           k = list.get(minimum);
           list.set(minimum, list.get(i));
-          list.set(i, list.get(minimum));
+          list.set(i, k);
         }
     //lines with your code
     System.out.println();
@@ -73,6 +73,26 @@ public class Sorts{
    */
   public void insertionSort(ArrayList <Integer> list){
     //replace these lines with your code
+    for( int i = 0 ;i < list.size() ; i++ ) {
+    /*storing current element whose left side is checked for its 
+             correct position .*/
+
+      int temp = list.get(i);    
+      int j = i;
+
+       /* check whether the adjacent element in left side is greater or
+            less than the current element. */
+
+          while(  j > 0  && temp < list.get(j-1)) {
+
+           // moving the left side element to one position forward.
+                list.set(j, list.get(j-1));   
+                j= j - 1;
+
+           }
+         // moving current element to its  correct position.
+           list.set(j, temp);       
+     }  
     System.out.println();
     System.out.println("Insertion Sort");
     System.out.println();
